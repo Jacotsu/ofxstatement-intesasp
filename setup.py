a1f3495e0531551af6d8ae6f7d9a0e719550b0d7
Jacotsu <Jacotsu@users.noreply.github.com>
2022-04-30 20:49:26 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup
 from setuptools import find_packages
 from distutils.core import setup
 
-version = "0.1.4"
+version = "0.1.5"
 
 with open('README.rst') as f:
     long_description = f.read()
